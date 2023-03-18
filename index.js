bebffa28d13b40924ace819b1d0e43d727f165cd
Victor Areiza <victorareiza02@gmail.com>
2023-03-18 00:28:10 -0500

    changes to phrases

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const button = document.getElementById("button");
 
 const messages = [
-  "Cuando pierdas, no pierdas la lección - Dalai Lama",
-  "No busques los errores, busca un remedio - Henry Ford",
-  "La vida es una aventura, atrévete - Teresa de Calcuta",
-  "Tu actitud, no tu aptitud, determinará tu altitud - Zig Ziglar",
-  "Tienes que hacer las cosas que crees que no puedes hacer - Eleanor Roosevelt",
-  "Si te caíste ayer, levántate hoy - H. G. Wells",
-  "Siempre parece imposible... hasta que se hace - Nelson Mandela",
-  "Si no pierdes, no puedes disfrutar de las victorias - Rafael Nadal",
-  "No dejes que el miedo se interponga en tu camino - Babe Ruth",
+  "...Cuando pierdas, no pierdas la lección - Dalai Lama",
+  "...No busques los errores, busca un remedio - Henry Ford",
+  "...La vida es una aventura, atrévete - Teresa de Calcuta",
+  "...Tu actitud, no tu aptitud, determinará tu altitud - Zig Ziglar",
+  "...Tienes que hacer las cosas que crees que no puedes hacer - Eleanor Roosevelt",
+  "...Si te caíste ayer, levántate hoy - H. G. Wells",
+  "...Siempre parece imposible... hasta que se hace - Nelson Mandela",
+  "...Si no pierdes, no puedes disfrutar de las victorias - Rafael Nadal",
+  "...No dejes que el miedo se interponga en tu camino - Babe Ruth",
 ];
 
 const load = document.querySelector(".load");
-load.innerHTML = "CARGANDO........"
+load.innerHTML = "CARGANDO..............."
 
 function showTitle() {
   const titleEl = document.querySelector(".title");
